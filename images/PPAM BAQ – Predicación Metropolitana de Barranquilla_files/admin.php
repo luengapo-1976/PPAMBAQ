55d608f3c650c76edf4df01f01d969521632e98e
LITOPLASP01\lgarcia <lgarcia@litoplas.com>
2026-08-23 22:27:32 -0500

    Auto-generated commit

---
+++ images/PPAM BAQ – Predicación Metropolitana de Barranquilla_files/admin.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+	<html lang="es">
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Acceder < PPAM BAQ — WordPress</title>
+	<meta name='robots' content='noindex, nofollow, noarchive' />
+<link rel='stylesheet' id='dashicons-css' href='https://ppam.com.co/wp-includes/css/dashicons.min.css?ver=6.8.2' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css' href='https://ppam.com.co/wp-includes/css/buttons.min.css?ver=6.8.2' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css' href='https://ppam.com.co/wp-admin/css/forms.min.css?ver=6.8.2' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css' href='https://ppam.com.co/wp-admin/css/l10n.min.css?ver=6.8.2' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css' href='https://ppam.com.co/wp-admin/css/login.min.css?ver=6.8.2' type='text/css' media='all' />
+	<meta name='referrer' content='strict-origin-when-cross-origin' />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="icon" href="https://ppam.com.co/wp-content/uploads/2022/06/cropped-LOGO-1-32x32.png" sizes="32x32" />
+<link rel="icon" href="https://ppam.com.co/wp-content/uploads/2022/06/cropped-LOGO-1-192x192.png" sizes="192x192" />
+<link rel="apple-touch-icon" href="https://ppam.com.co/wp-content/uploads/2022/06/cropped-LOGO-1-180x180.png" />
+<meta name="msapplication-TileImage" content="https://ppam.com.co/wp-content/uploads/2022/06/cropped-LOGO-1-270x270.png" />
+	</head>
+	<body class="login no-js login-action-login wp-core-ui  locale-es-es">
+	<script type="text/javascript">
+/* <![CDATA[ */
+document.body.className = document.body.className.replace('no-js','js');
+/* ]]> */
+</script>
+
+				<h1 class="screen-reader-text">Acceder</h1>
+			<div id="login">
+		<h1 role="presentation" class="wp-login-logo"><a href="https://es.wordpress.org/">Funciona con WordPress</a></h1>
+	
+		<form name="loginform" id="loginform" action="https://ppam.com.co/wp-login.php" method="post">
+			<p>
+				<label for="user_login">Nombre de usuario o correo electrónico</label>
+				<input type="text" name="log" id="user_login" class="input" value="zullymejia" size="20" autocapitalize="off" autocomplete="username" required="required" />
+			</p>
+
+			<div class="user-pass-wrap">
+				<label for="user_pass">Contraseña</label>
+				<div class="wp-pwd">
+					<input type="password" name="pwd" id="user_pass" class="input password-input" value="" size="20" autocomplete="current-password" spellcheck="false" required="required" />
+					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Mostrar la contraseña">
+						<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> <label for="rememberme">Recuérdame</label></p>
+			<p class="submit">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Acceder" />
+									<input type="hidden" name="redirect_to" value="https://ppam.com.co/wp-admin/admin.php?page=stats&amp;noheader&amp;proxy&amp;chart=admin-bar-hours-scale" />
+									<input type="hidden" name="testcookie" value="1" />
+			</p>
+		</form>
+
+					<p id="nav">
+				<a class="wp-login-lost-password" href="https://ppam.com.co/wp-login.php?action=lostpassword">¿Has olvidado tu contraseña?</a>			</p>
+			<script type="text/javascript">
+/* <![CDATA[ */
+function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_pass" ); d.value = "";d.focus(); d.select();} catch( er ) {}}, 200);}
+wp_attempt_focus();
+if ( typeof wpOnload === 'function' ) { wpOnload() }
+/* ]]> */
+</script>
+		<p id="backtoblog">
+			<a href="https://ppam.com.co/">&larr; Ir a PPAM BAQ</a>		</p>
+		<div class="privacy-policy-page-link"><a class="privacy-policy-link" href="https://ppam.com.co/politica-privacidad/" rel="privacy-policy">POLÍTICA DE TRATAMIENTO Y PROTECCIÓN DE DATOS PERSONALES</a></div>	</div>
+				<div class="language-switcher">
+				<form id="language-switcher" method="get">
+
+					<label for="language-switcher-locales">
+						<span class="dashicons dashicons-translation" aria-hidden="true"></span>
+						<span class="screen-reader-text">
+							Idioma						</span>
+					</label>
+
+					<select name="wp_lang" id="language-switcher-locales"><option value="en_US" lang="en" data-installed="1">English (United States)</option>
+<option value="es_CO" lang="es" data-installed="1">Español de Colombia</option>
+<option value="es_ES" lang="es" selected='selected' data-installed="1">Español</option></select>
+					
+											<input type="hidden" name="redirect_to" value="https://ppam.com.co/wp-admin/admin.php?page=stats&noheader&proxy&chart=admin-bar-hours-scale" />
+					
+					
+						<input type="submit" class="button" value="Cambiar">
+
+					</form>
+				</div>
+			
+	<script type="text/javascript" src="https://ppam.com.co/wp-includes/js/clipboard.min.js?ver=2.0.11" id="clipboard-js"></script>
+<script type="text/javascript" src="https://ppam.com.co/wp-includes/js/jquery/jquery.min.js?ver=3.7.1" id="jquery-core-js"></script>
+<script type="text/javascript" src="https://ppam.com.co/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.4.1" id="jquery-migrate-js"></script>
+<script type="text/javascript" id="zxcvbn-async-js-extra">
+/* <![CDATA[ */
+var _zxcvbnSettings = {"src":"https:\/\/ppam.com.co\/wp-includes\/js\/zxcvbn.min.js"};
+/* ]]> */
+</script>
+<script type="text/javascript" src="https://ppam.com.co/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
+<script type="text/javascript" src="https://ppam.com.co/wp-includes/js/dist/hooks.min.js?ver=4d63a3d491d11ffd8ac6" id="wp-hooks-js"></script>
+<script type="text/javascript" src="https://ppam.com.co/wp-includes/js/dist/i18n.min.js?ver=5e580eb46a90c2b997e6" id="wp-i18n-js"></script>
+<script type="text/javascript" id="wp-i18n-js-after">
+/* <![CDATA[ */
+wp.i18n.setLocaleData( { 'text direction\u0004ltr': [ 'ltr' ] } );
+/* ]]> */
+</script>
+<script type="text/javascript" id="password-strength-meter-js-extra">
+/* <![CDATA[ */
+var pwsL10n = {"unknown":"Fortaleza de la contrase\u00f1a desconocida","short":"Muy d\u00e9bil","bad":"D\u00e9bil","good":"Medio","strong":"Fuerte","mismatch":"No coinciden"};
+/* ]]> */
+</script>
+<script type="text/javascript" id="password-strength-meter-js-translations">
+/* <![CDATA[ */
+( function( domain, translations ) {
+	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
+	localeData[""].domain = domain;
+	wp.i18n.setLocaleData( localeData, domain );
+} )( "default", {"translation-revision-date":"2025-09-09 19:08:59+0000","generator":"GlotPress\/4.0.1","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n != 1;","lang":"es"},"%1$s is deprecated since version %2$s! Use %3$s instead. Please consider writing more inclusive code.":["\u00a1%1$s est\u00e1 obsoleto desde la versi\u00f3n %2$s! Usa %3$s en su lugar. Por favor, plant\u00e9ate escribir un c\u00f3digo m\u00e1s inclusivo."]}},"comment":{"reference":"wp-admin\/js\/password-strength-meter.js"}} );
+/* ]]> */
+</script>
+<script type="text/javascript" src="https://ppam.com.co/wp-admin/js/password-strength-meter.min.js?ver=6.8.2" id="password-strength-meter-js"></script>
+<script type="text/javascript" src="https://ppam.com.co/wp-includes/js/underscore.min.js?ver=1.13.7" id="underscore-js"></script>
+<script type="text/javascript" id="wp-util-js-extra">
+/* <![CDATA[ */
+var _wpUtilSettings = {"ajax":{"url":"\/wp-admin\/admin-ajax.php"}};
+/* ]]> */
+</script>
+<script type="text/javascript" src="https://ppam.com.co/wp-includes/js/wp-util.min.js?ver=6.8.2" id="wp-util-js"></script>
+<script type="text/javascript" src="https://ppam.com.co/wp-includes/js/dist/dom-ready.min.js?ver=f77871ff7694fffea381" id="wp-dom-ready-js"></script>
+<script type="text/javascript" id="wp-a11y-js-translations">
+/* <![CDATA[ */
+( function( domain, translations ) {
+	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
+	localeData[""].domain = domain;
+	wp.i18n.setLocaleData( localeData, domain );
+} )( "default", {"translation-revision-date":"2025-09-29 19:09:09+0000","generator":"GlotPress\/4.0.1","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n != 1;","lang":"es"},"Notifications":["Avisos"]}},"comment":{"reference":"wp-includes\/js\/dist\/a11y.js"}} );
+/* ]]> */
+</script>
+<script type="text/javascript" src="https://ppam.com.co/wp-includes/js/dist/a11y.min.js?ver=3156534cc54473497e14" id="wp-a11y-js"></script>
+<script type="text/javascript" id="user-profile-js-extra">
+/* <![CDATA[ */
+var userProfileL10n = {"user_id":"0","nonce":"22b861bd10"};
+/* ]]> */
+</script>
+<script type="text/javascript" id="user-profile-js-translations">
+/* <![CDATA[ */
+( function( domain, translations ) {
+	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
+	localeData[""].domain = domain;
+	wp.i18n.setLocaleData( localeData, domain );
+} )( "default", {"translation-revision-date":"2025-09-09 19:08:59+0000","generator":"GlotPress\/4.0.1","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n != 1;","lang":"es"},"Application password has been copied to your clipboard.":["Se ha copiado la contrase\u00f1a de aplicaci\u00f3n al portapapeles."],"Your new password has not been saved.":["No ha sido guardada tu nueva contrase\u00f1a."],"Hide":["Ocultar"],"Confirm use of weak password":["Confirma el uso de una contrase\u00f1a d\u00e9bil."],"Hide password":["Ocultar la contrase\u00f1a"],"Show password":["Mostrar la contrase\u00f1a"],"Show":["Mostrar"],"The changes you made will be lost if you navigate away from this page.":["Los cambios realizados se perder\u00e1n si sales de esta p\u00e1gina."]}},"comment":{"reference":"wp-admin\/js\/user-profile.js"}} );
+/* ]]> */
+</script>
+<script type="text/javascript" src="https://ppam.com.co/wp-admin/js/user-profile.min.js?ver=6.8.2" id="user-profile-js"></script>
+	<script>
+	/(trident|msie)/i.test(navigator.userAgent)&&document.getElementById&&window.addEventListener&&window.addEventListener("hashchange",function(){var t,e=location.hash.substring(1);/^[A-z0-9_-]+$/.test(e)&&(t=document.getElementById(e))&&(/^(?:a|select|input|button|textarea)$/i.test(t.tagName)||(t.tabIndex=-1),t.focus())},!1);
+	</script>
+		</body>
+	</html>
+	
